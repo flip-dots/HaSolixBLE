97cdc21ca75e1d88373b3473af85b881b2ca2dc5
Harvey Lelliott <42912136+flip-dots@users.noreply.github.com>
2026-02-08 15:39:08 +0000

    Treat X and non-X models as the same device

--- custom_components/solix_ble/config_flow.py
+++ custom_components/solix_ble/config_flow.py
@@ -45,9 +45,9 @@ async def validate_input(hass: HomeAssistant, address: str) -> None:
 
     try:
         device = None
-        if ble_device.name == "Anker SOLIX C300X":
+        if "Anker SOLIX C300" in ble_device.name:
             device = C300(ble_device)
-        elif ble_device.name == "Anker SOLIX C1000":
+        elif "Anker SOLIX C1000" in ble_device.name:
             device = C1000(ble_device)
         else:
             _LOGGER.warning(
