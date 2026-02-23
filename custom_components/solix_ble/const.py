ccd3467b62b18db79496e5bbe86a621b5e4e7f82
Harvey Lelliott <42912136+flip-dots@users.noreply.github.com>
2026-02-23 12:00:15 +0000

    Use device selector in config flow instead of relying on device name

--- custom_components/solix_ble/const.py
+++ custom_components/solix_ble/const.py
@@ -1,7 +1,15 @@
 """Constants for the SolixBLE integration."""
 
+from enum import Enum
+
 DOMAIN = "solix_ble"
 
 PORT_STATUS_STRINGS = ["Unknown", "Not connected", "Output", "Input"]
 
 LIGHT_STATUS_STRINGS = ["Unknown", "Off", "Low", "Medium", "High"]
+
+
+class Models(Enum):
+    C300 = "C300(X)"
+    C1000 = "C1000(X)"
+    UNKNOWN = "Unknown"
